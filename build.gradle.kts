8dc6ba4eb229ad960598956f18718a7e76203964
Xe-iu <xe_iu@outlook.com>
2026-03-15 04:13:08 +0800

    Update App Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 extra["appPackageName"] = "io.github.proify.lyricon"
 extra["appVersionCode"] = 21
-extra["appVersionName"] = "1.0.30-beta2-fixSetting"
+extra["appVersionName"] = "1.0.30-beta2-edit"
 extra["compileSdkVersion"] = 36
 extra["targetSdkVersion"] = 36
 extra["minSdkVersion"] = 27
